065b1f512ac932db4c74f01af3a790f4229dfedd
August Wester <august.wester@gmail.com>
2020-08-26 12:45:36 +0200

    Remove old solution

--- matching/solution.py
+++ matching/solution.py
@@ -7,47 +7,65 @@ class Person(object):
         self.name = name.rsplit()[0]
         self.prefs = None
         self.partner = None
+        self.is_proposer = self.id % 2 == 0
 
-mw = []
+    def set_prefs(self, prefs):
+        prefs =  [int(pref) - 1 for pref in prefs]
+        if self.is_proposer:
+            self.prefs = prefs
+        else:
+            self.prefs = {id: n-rank for (rank, id) in enumerate(prefs)}
+
+persons = []
 free_m = []
-traversing_names = True
 n = 0
 
-for (linenumber, line) in enumerate(stdin):
-    if line[0] == "#":
-        pass
-    elif line[0] == "n":
-        n = int(line[2:])
-    elif line == "\n":
-        traversing_names = False
-    elif traversing_names:
-        person = Person(line)
-        mw.append(person)
-        if person.id % 2 == 0: free_m.append(person.id)
-    elif not traversing_names:
+def parse_preferences(start_index):
+    for (_, line) in enumerate(stdin, start_index):
         split = line.replace(" \n", "").split(" ")
         id = int(split[0][:-1]) - 1
         prefs = split[1:]
-        prefs = [int(pref) - 1 for pref in prefs]
-        if id % 2 == 1:
-            prefs = {id: n-rank for (rank, id) in enumerate(prefs)}
-        mw[id].prefs = prefs
+        person = persons[id]
+        person.set_prefs(prefs)
 
-while len(free_m) > 0 and len(mw[free_m[0]].prefs) > 0:
-    m = mw[free_m[0]]
-    w = mw[m.prefs.pop(0)]
+def parse_names(start_index):
+    for (line_number, line) in enumerate(stdin, start_index):
+        if line == "\n":
+            parse_preferences(line_number)
+            break
+        else:
+            person = Person(line)
+            persons.append(person)
+            if person.is_proposer: free_m.append(person.id)
+def parse_start():     
+    for (line_number, line) in enumerate(stdin):
+        if line[0] == "#":
+            pass
+        elif line[0] == "n":
+            n = int(line[2:])
+            parse_names(line_number)
+        else: break
 
-    if w.partner is None:
-        w.partner = m
-        m.partner = w
-        free_m.pop(0)
-    elif w.prefs[m.id] > w.prefs[w.partner.id]:
-        free_m.pop(0)
-        free_m.append(w.partner.id)
-        w.partner = m
-        m.partner = w
+def run_Gale_Shapley():
+    while len(free_m) > 0 and len(persons[free_m[0]].prefs) > 0:
+        m = persons[free_m[0]]
+        w = persons[m.prefs.pop(0)]
 
-for p in mw:
-    if p.id % 2 == 0:
-        print(p.name + " -- " + p.partner.name)
+        if w.partner is None:
+            w.partner = m
+            m.partner = w
+            free_m.pop(0)
+        elif w.prefs[m.id] > w.prefs[w.partner.id]:
+            free_m.pop(0)
+            free_m.append(w.partner.id)
+            w.partner = m
+            m.partner = w
 
+def print_result():
+    for p in persons:
+        if p.id % 2 == 0:
+            print(p.name + " -- " + p.partner.name)
+
+parse_start()
+run_Gale_Shapley()
+print_result()
