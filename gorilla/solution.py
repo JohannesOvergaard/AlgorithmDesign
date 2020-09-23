2cb8868b8365d86958364f68ce23f86984433767
Martin Winther <marw@itu.dk>
2020-09-23 14:10:45 +0200

    Comments

--- gorilla/solution.py
+++ gorilla/solution.py
@@ -6,8 +6,13 @@ class Organism(object):
         self.gene = ""
 
 def read_penalties(filename):
+    """
+    Parses a file in the format of BLOSUM62.txt and creates a dictionary 
+    mapping a pair of letters to their associated penalty
+    filename: the location of the file to read
+    """
     file = open(filename)
-
+    
     penalties = {}
     letters = []
     for index,line in enumerate(file):
@@ -15,6 +20,7 @@ def read_penalties(filename):
             continue
         split = line.rsplit()
         if index == 6:
+            #Penalties is actually a dictionary of dictionaries! What!
             penalties = {x:{} for x in split}
             letters = split
             continue
@@ -24,6 +30,9 @@ def read_penalties(filename):
     return penalties
 
 def parse_input():
+    """
+    Parses organisms from standardin and creates a list of Organism objects each containing a name and their genetic sequence as a string
+    """
     organisms = []
     current_organism = None
     for line in stdin:
@@ -38,15 +47,26 @@ def parse_input():
 def gap_penalty(letter):
     return penalties["*"][letter]
 
+
 def construct_table(o1, o2, penalties):
+    """
+    Using the naive iterative implementation (bottom up), constructs
+    a table of m n elements containing optimal penalties for the match
+    between two organisms
+    o1: the first organism
+    o2: the second organism
+    penalties: a dictionary of penalties
+    """
     m,n = len(o1.gene), len(o2.gene)
     M = [[0 for _ in range(n+1)] for _ in range(m+1) ]
     
+    #Filling the first row and column with successively larger gap penalties
     for i in range(0, m):
         M[i+1][0] = M[i][0] + gap_penalty(o1.gene[i]) 
     for j in range(0, n):
         M[0][j+1] = M[0][j] + gap_penalty(o2.gene[j]) 
 
+    #For each cell, choose the optimal penalty
     for i in range(m):
         for j in range(n):
             penalty = penalties[o1.gene[i]][o2.gene[j]]
@@ -58,19 +78,26 @@ def construct_table(o1, o2, penalties):
     return M
 
 def trace_back(M, o1, o2, penalties):
+    """
+    Reconstructing the optimal alignment by tracing back through the table,
+    selecting the previous cell that led to the current one.
+    """
     i, j = len(o1.gene), len(o2.gene)
     results_o1, results_o2 = "", ""
 
     while i > 0 or j > 0:
+        #If alignment was preferred over a gap
         if M[i][j] == M[i-1][j-1] + penalties[o1.gene[i-1]][o2.gene[j-1]] :
             i-=1
             j-=1
             results_o1 += o1.gene[i]
             results_o2 += o2.gene[j]
+        #If a gap was introduced on the second gene
         elif M[i][j] == M[i-1][j] + gap_penalty(o1.gene[i-1]):
             i -= 1
             results_o1 += o1.gene[i]
             results_o2 += "-"
+        #If a gap was introduced on the first gene
         else:
             j-=1
             results_o1 += "-"
@@ -82,6 +109,8 @@ def trace_back(M, o1, o2, penalties):
 penalties = read_penalties("data/BLOSUM62.txt")
 organisms = parse_input()
 
+#Loop through every organism and match it with every other organism that hasn't 
+#already matched with it
 for i in range(len(organisms)):
     for j in range(i+1, len(organisms)):
         M = construct_table(organisms[i], organisms[j], penalties)
