8747a93b211a7d1d62e17d9efbe587efc68bf1d4
August Wester <august.wester@gmail.com>
2020-10-28 14:57:08 +0100

    Restructuring

---
+++ red-scare/Pathfinding.py
@@ -0,0 +1,76 @@
+import queue
+
+def bfs2(G, filt, end_condition):
+    s, t = G.s, G.t
+    visited = {}
+    q = queue.Queue()
+
+    for n in G.s.neighbors:
+        if filt(s, n):
+            q.put((s,n))
+
+    while not q.empty():
+        u,v = q.get()
+        visited[v] = u 
+        for n in v.neighbors:
+            if n not in visited and filt(v,n):
+                q.put((v,n))
+        if end_condition(v):
+            path, curr = [v], v
+            while curr != s:
+                path.append(visited[curr])
+                curr = visited[curr]
+            path.reverse()
+            return path, visited
+    
+    return None
+
+def bfs(G, filt):
+    s, t = G.s, G.t
+    visited = {}
+    q = queue.Queue()
+
+    for n in G.s.neighbors:
+        if filt(s, n):
+            q.put((s,n))
+
+    while not q.empty():
+        u,v = q.get()
+        visited[v] = u 
+        for n in v.neighbors:
+            if n not in visited and filt(v,n):
+                q.put((v,n))
+        if v == t:
+            path, curr = [t], t
+            while curr != s:
+                path.append(visited[curr])
+                curr = visited[curr]
+            path.reverse()
+            return path
+    
+    return None
+
+def dijkstra(G, cost):
+    s, t = G.s, G.t
+    visited = {}
+    q = queue.PriorityQueue()
+
+    for n in G.s.neighbors:
+        q.put((cost(s, n), s, n))
+
+    while not q.empty():
+        c, u, v = q.get()
+        visited[v] = (u, c) 
+        for n in v.neighbors:
+            if n not in visited or n == t:
+                q.put((cost(v, n) + c, v, n))
+        if v == t:
+            path, curr = [t], t
+            while curr != s:
+                path.append(visited[curr][0])
+                curr = visited[curr][0]
+            path.reverse()
+            return path
+    
+    return None
+
--- red-scare/run.py
+++ red-scare/run.py
@@ -1,10 +1,18 @@
 from Parser import parse
-from BFS import bfs
+from Pathfinding import bfs, dijkstra
 
-def noneProblem(G):
+def none(G):
     p = bfs(G, lambda x, y: not y.is_red or y == G.t)
     #for n in p: print(n.id)
     return -1 if p is None else len(p)
+
+def some(G):
+
+def many(G): pass
+
+def few(G):
+    p = dijkstra(G, lambda x, y: 1 if y.is_red else 0)
+    return -1 if p is None else len([x for x in p if x.is_red])
 
 def alternating(G):
     p = bfs(G, lambda x, y: x.is_red != y.is_red)
@@ -12,5 +20,7 @@ def alternating(G):
     return p != None
 
 G = parse()
-print(noneProblem(G))
+print(none(G))
 print(alternating(G))
+print(few(G))
+print(some(G))
